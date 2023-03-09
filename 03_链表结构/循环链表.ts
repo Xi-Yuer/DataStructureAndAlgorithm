8872275a438b5cfe00061d3a8f2aae0479d4e44a
Xi-Yuer <2214380963@qq.com>
2023-03-09 13:12:30 +0800

    循环链表

--- 03_链表结构/循环链表.ts
+++ 03_链表结构/循环链表.ts
@@ -1,6 +1,35 @@
 import { LinkList } from "./单项链表";
 
 // 循环链表（链表中最后一个元素的next指向的是头部）
-class CircularList<T> extends LinkList<T> {
-    
+export class CircularList<T> extends LinkList<T> {
+    // 重新实现的方法
+    append(el: T): void {
+        super.append(el)
+        // 拿到最后一个节点，将他的 next 指向第一个节点
+        this.tail!.next = this.head
+    }
+    insert(el: T, position: number): boolean {
+        const isSuccess = super.insert(el, position)
+        // 当插入的节点在最后一个位置或者第一个位置时时(需要将最后一个节点的 next 指向头结点)
+        if (isSuccess && position === this.length - 1 || position === 0) {
+            this.tail!.next = this.head
+        }
+        return isSuccess
+    }
+
+    removeAt(position: number): T | null {
+        const value = super.removeAt(position)
+        // 当删除的是第一个节点时(需要改变一下 tail 的指向)
+        if (value && this.tail && position === 0 || position === this.length) {
+            this.tail!.next = this.head
+        }
+        return value
+    }
 }
+const circularList = new CircularList<string>()
+circularList.append('a')
+circularList.append('b')
+circularList.append('c')
+circularList.append('d')
+
+circularList.traverse()
